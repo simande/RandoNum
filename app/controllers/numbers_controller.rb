fec7f10e19f50a558c9dfbafd3de58f700a14c6e
Eric <e@simande.com>
2011-09-28 13:13:00 -0400

    s

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,6 +1,6 @@
 class NumbersController < ApplicationController
   def index
     @number = Number.latest
-    @users = User.select("email, num_logins")
+    @users = User.select("email, num_logins").order("num_logins DESC")
   end
 end
